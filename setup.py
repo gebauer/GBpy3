85d26fad3f5495ddbc1dc42354e4dcb82dad910b
Unknown <mail@jan-gebauer.de>
2017-02-03 15:51:04 +0100

    Move to GBpy3

--- setup.py
+++ setup.py
@@ -1,46 +1,112 @@
-import sys
-import os
-from setuptools import setup
+"""A setuptools based setup module.
 
-main_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(main_dir, "GBpy"))
-import GBpy
-# del sys.path[0]
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+here = path.abspath(path.dirname(__file__))
 
-setup(name='GBpy3',
-    version='0.1.2',
-    author='Arash Dehghan Banadaki, Srikanth Patala',
-    author_email='adehgha@ncsu.edu, spatala@ncsu.edu',
-    description="GBpy is an opensource python package for calculating the geometric properties of interfaces in crystals.",
-    long_description=read('README.rst'),
-    url='https://github.com/adehgha/GBpy',
-    download_url = 'https://github.com/adehgha/GBpy/tarball/0.1.2',
-    platforms='any',
-    requires=['numpy', 'scipy'],
-    keywords = ['bicrystallography', 'interfaces', 'grain boundaries'],
-    classifiers=['Development Status :: 2 - Pre-Alpha', 'Topic :: Utilities'],
-    license='License :: GNU-GPL',
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='GBpy3',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.3',
+
+    description='GBpy3 is an opensource python package for calculating the geometric properties of'
+                'interfaces in crystals adapted for Python 3.',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/gebauer/GBpy3',
+
+    # Author details
+    author='Arash Dehghan Banadaki, Srikanth Patala, Jan Gebauer (conversion to Py3 only)',
+    author_email='adehgha@ncsu.edu, spatala@ncsu.edu, jan.gebauer@uni-koeln.de',
+
+    # Choose your license
+    license='GNU-GPL',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='Biocrystallography, interfaces, grain boundaries',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['build', 'docs', 'dist', 'GBPy3.egg-info']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy', 'scipy'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'GBpy3': ['pkl_files/*', '*.pkl'],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
 )
-
-# from distutils.core import setup
-# with open('README.rst') as f:
-#     long_description = f.read()
-
-# setup(name='GBpy',
-#     version='0.1.0',
-#     author='Arash Dehghan Banadaki, Srikanth Patala',
-#     author_email='adehgha@ncsu.edu, spatala@ncsu.edu',
-#     description="GBpy is an opensource python package for calculating the geometric properties of interfaces in crystals.",
-#     long_description=long_description,
-#     url='https://github.com/adehgha/GBpy',
-#     download_url = 'https://github.com/adehgha/GBpy/tarball/0.1.0',
-#     platforms='any',
-#     requires=['numpy', 'scipy'],
-#     keywords = ['bicrystallography', 'interfaces', 'grain boundaries'],
-#     classifiers=['Development Status :: 2 - Pre-Alpha', 'Topic :: Utilities'],
-#     license='License :: GNU-GPL',
-# )
